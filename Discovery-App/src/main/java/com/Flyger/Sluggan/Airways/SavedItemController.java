b5d0e586a319b77147321cd02b3958caa78140de
Mrsluggan <ericflyger@gmail.com>
2024-02-04 18:24:49 +0100

    small fixes

--- Discovery-App/src/main/java/com/Flyger/Sluggan/Airways/SavedItemController.java
+++ Discovery-App/src/main/java/com/Flyger/Sluggan/Airways/SavedItemController.java
@@ -8,12 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SavedItemController {
 
-    @Autowired
-    private RemoveItemRepository removeItemRepository;
-
     @GetMapping()
-    public String getIndex(Model model) {
-        model.addAttribute("removedTodos", removeItemRepository.findAll());
+    public String getIndex( ) {
         return "index";
     }
 
