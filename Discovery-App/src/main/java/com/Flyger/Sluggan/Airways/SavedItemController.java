3a8dcb4c52fb6862365d6532aeb8d784399b36c3
Mrsluggan <ericflyger@gmail.com>
2024-02-05 08:42:23 +0100

    small fixes

--- Discovery-App/src/main/java/com/Flyger/Sluggan/Airways/SavedItemController.java
+++ Discovery-App/src/main/java/com/Flyger/Sluggan/Airways/SavedItemController.java
@@ -1,8 +1,6 @@
 package com.Flyger.Sluggan.Airways;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
